594e14bd797bae770f4dc11dbe4b0c4d16427740
sergey.boronin@rt.ru <bo-ronin@yandex.ru>
2022-06-26 12:33:06 +0300

    added typo & color vars

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import { AppProps } from "../node_modules/next/dist/shared/lib/router/router";
+import Head from "../node_modules/next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>TopApp</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
