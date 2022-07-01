b0c8b8dcd31838caeec6fd727f0a0d0c732c8a66
sergey.boronin@rt.ru <bo-ronin@yandex.ru>
2022-07-01 15:41:35 +0300

    updated svg processing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
-import { Head } from "../node_modules/next/document";
+import { Htag } from "../components/index";
+import { Button } from "../components/Button/Button";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Htag tag="h1">text</Htag>
+      <Button appearance={"primary"}>Кнопка</Button>
+      <Button appearance={"ghost"}>Кнопка</Button>
+    </div>
+  );
 }
